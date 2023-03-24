587395c4ea6b9478a64864bcdd0a40d37204b561
Srihari1798 <120098197+Srihari1798@users.noreply.github.com>
2023-03-24 22:44:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+// Get all the elements that store the prices of the items in the list
+const priceElements = document.querySelectorAll('[data-ns-test="prices"]');
+
+// Calculate the sum of prices
+let totalPrice = 0;
+for (let i = 0; i < priceElements.length; i++) {
+  totalPrice += parseFloat(priceElements[i].textContent);
+}
+
+// Create a new row for the total price
+const totalRow = document.createElement('tr');
+const totalCell = document.createElement('td');
+totalCell.setAttribute('colspan', '2');
+totalCell.textContent = `Total: $${totalPrice.toFixed(2)}`;
+totalCell.setAttribute('data-ns-test', 'grandTotal');
+totalRow.appendChild(totalCell);
+
+// Add the total row to the table
+const table = document.querySelector('table');
+table.appendChild(totalRow);
